32bab39f15c077d1a8717c2a2d8f1192901140a5
Yogesh Agrawal <agrawalyogesh5047@gmail.com>
2024-02-16 03:04:11 +0530

    ready to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen p-4">
+    <main className="flex justify-center p-4">
       <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://github.com/yogesh0509"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-7 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Image
+            src={`/github.png`}
+            width={400}
+            height={400}
+            alt="github"
+            className='p-8'
+          />
           <h2 className={`mb-3 text-2xl font-semibold`}>
             GitHub{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -17,16 +24,24 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Find my GitHub profile which stores all the repositories in which I have worked.
           </p>
         </a>
 
         <a
           href="https://www.linkedin.com/in/yogesh0509/"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-7 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Image
+            src={`/linkedin.png`}
+            width={400}
+            height={400}
+            alt="github"
+            className='p-8'
+          />
+
           <h2 className={`mb-3 text-2xl font-semibold`}>
             LinkedIn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -34,16 +49,23 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Visit my LinkedIn profile to connect with me.
           </p>
         </a>
 
         <a
           href="https://drive.google.com/file/d/1MuPnWQruBK_T1c1OJ_s0qMPVCY9ZoRac/view?usp=sharing"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-7 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Image
+            src={`/resume.png`}
+            width={400}
+            height={400}
+            alt="github"
+            className='p-8'
+          />
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Resume{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -51,16 +73,23 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+            Check out my professional resume
           </p>
         </a>
 
         <a
           href="./"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-lg border border-transparent px-7 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Image
+            src={`/portfolio.png`}
+            width={400}
+            height={400}
+            alt="github"
+            className='p-8'
+          />
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Portfolio{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -68,7 +97,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Link to visit and explore my portfolio website.
           </p>
         </a>
       </div>
